241332acb248c2e5116c527ea92c8c6060265f75
PabloSan1997 <jos_pab_997@hotmali.com>
2023-05-02 16:10:51 -0600

    Se agrego cors y se iniico con admin

--- Servidor/index.js
+++ Servidor/index.js
@@ -2,10 +2,11 @@ const express = require("express");
 const { conection } = require("./database/config");
 const { crearApi } = require("./routers");
 const { boomHandle } = require("./middlewares/boomHandle");
-
+const cors = require("cors");
 const app = express();
 
 app.use(express.json());
+app.use(cors());
 conection();
 crearApi(app);
 app.use(boomHandle)
