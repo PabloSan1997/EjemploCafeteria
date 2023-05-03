d3efa24336c614cdb8ef5e1a08234ff0fe311f08
PabloSan1997 <jos_pab_997@hotmali.com>
2023-05-02 21:21:20 -0600

    Se modifico server

--- Servidor/database/config.js
+++ Servidor/database/config.js
@@ -4,8 +4,6 @@ const { variables } = require("./envVariables");
 const URL = `mongodb+srv://${variables.usuario}:${variables.password}@cafecluster.pbkngux.mongodb.net/${variables.db_name}?retryWrites=true&w=majority`;
 
 function conection() {
-  Moongose.connect(URL, { useNewUrlParser: true, useUnifiedTopology: true })
-    .then((ele) => console.log("conectado a la base de datos"))
-    .catch((ele) => console.log("hay un error con la base de datos"));
+  Moongose.connect(URL, { useNewUrlParser: true, useUnifiedTopology: true });
 }
 module.exports = { conection };
